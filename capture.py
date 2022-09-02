102fdb978e51ce2a09a3d3d28f4b5513cb666b22
deltaryz <wincam97@gmail.com>
2022-09-01 21:42:22 -0500

    Comment code

--- capture.py
+++ capture.py
@@ -12,16 +12,21 @@ import os
 import time
 from time import localtime, strftime
 
+# Use slop to select a region
 region = subprocess.check_output("slop", text=True, shell=True)
-coords = region.strip().split("+",1)
 
+# Split strings to get separate numbers
+coords = region.strip().split("+",1)
 size = coords[0].split("x")
 offset = coords[1].split("+")
 
+# Get current time for video filename
 ttime = strftime("%Y-%m-%d %H.%M.%S", localtime())
 
+# Get the current directory the script is running from
 path = os.path.abspath(os.path.dirname(__file__))
 
+# Command flags for ffmpeg
 command = ( "ffmpeg "
             f"-video_size {coords[0]} " 
             "-framerate 60 "
@@ -29,19 +34,22 @@ command = ( "ffmpeg "
             "-show_region 1 "
             f"-i :0.0+{offset[0]},{offset[1]} "
             "-c:v libvpx -b:v 2M "
-            f"-y \"{path}/{ttime}.webm\""
+            f"-y \"{path}/{ttime}.webm\"" # Video is saved next to the script
           )
 
+# Actually start ffmpeg
 ffmpeg = subprocess.Popen(command, shell=True)
 
+# Make sure the stop button is placed near the recording region
+screen_width, screen_height = sg.Window.get_screen_size()
 locationX = int(offset[0])
 locationY = int(offset[1]) + int(size[1])
 
-screen_width, screen_height = sg.Window.get_screen_size()
-
+# Move stop button above region if it's near the bottom
 if locationY > screen_height - 50:
     locationY = int(offset[1]) - 30
 
+# Stop button window properties
 event = sg.Window('Capture',
         [[sg.B('Stop')]],
         size=(50,30),
@@ -51,17 +59,24 @@ event = sg.Window('Capture',
         location=(locationX,locationY)
         )
 
+# Close window when the stop button is pressed
 event.read(close=True)
 
+# Tell ffmpeg to stop
 os.kill(ffmpeg.pid, signal.SIGINT)
 
+# Wait for ffmpeg to finish up
 ffmpeg.wait()
 
+# Curl command flags
 commandcurl = ( "curl "
                 f"--upload-file \"{path}/{ttime}.webm\" "
                 "https://transfer.sh"
               )
 
+# Run curl, uploading video to transfer.sh
 link = subprocess.check_output(commandcurl, text=True, shell=True)
+
+# Copy the link to clipboard and print
 print(link)
 os.system(f"echo \"{link}\" | xclip -i -selection clipboard")
